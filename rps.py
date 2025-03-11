aad716206d96b929ef76086d98b0e4ecdf1808e1
wdporter69 <williamdporter1969@gmail.com>
2025-03-11 07:32:56 -0500

    closure

--- rps.py
+++ rps.py
@@ -2,66 +2,82 @@ import sys
 import random
 from enum import Enum
 
-game_count = 0
+def rps():
+    game_count = 0
+    player_wins = 0
+    python_wins = 0
 
-def play_rps():
-    class RPS(Enum):
-        ROCK = 1
-        PAPER = 2
-        SCISSORS = 3
+    def play_rps():
+        nonlocal player_wins
+        nonlocal python_wins
+        class RPS(Enum):
+            ROCK = 1
+            PAPER = 2
+            SCISSORS = 3
 
-    playerchoice = input("\nEnter ...\n1 for Rock\n2 for Paper or \n3 for Scissors:\n\n")
+        playerchoice = input("\nEnter ...\n1 for Rock\n2 for Paper or \n3 for Scissors:\n\n")
 
-    if playerchoice not in["1", "2", "3"]:
-        print("You must enter 1, 2, or 3")
-        return play_rps()
-        
-    player = int(playerchoice)
-
-    computerchoice = random.choice("123")
-
-    computer = int(computerchoice)
-
-    print("")
-    print("You chose " + str(RPS(player)).replace('RPS.', '') + '.')
-    print("Python chose " + str(RPS(computer)).replace('RPS.', '') + '.')
-    print("")
-
-    def decide_winner(player, computer):        
-        if player == 1 and computer == 3:
-            return "🥳 You win!"
-        elif player == 2 and computer == 1:
-            return "🥳 You win!"
-        elif player == 3 and computer == 2:
-            return "🥳 You win!"
-        elif player == computer:
-            return "😲 Tie game!"
-        else:
-            return "🐍 Python wins!"
-        
-    game_result = decide_winner(player, computer)
-    
-    print(game_result)
-    
-    global game_count
-    game_count += 1
-    
-    print("\nGame count: " + str(game_count))
-    print("\n Play again? ")
-    
-    while True:
-        playagain = input("\n Y for Yes or \n Q for Quit \n")
-        if playagain.lower() not in ["y", "q"]:
-            continue 
-        else:
-            break
-        
-    if playagain.lower() == "y":
-        return play_rps()
-    else:
-        print("\n💕 Thank you for playing!\n")
-        sys.exit("Bye! 👋")
+        if playerchoice not in["1", "2", "3"]:
+            print("You must enter 1, 2, or 3")
+            return play_rps()
             
+        player = int(playerchoice)
 
+        computerchoice = random.choice("123")
 
-play_rps()
+        computer = int(computerchoice)
+
+        print("")
+        print("You chose " + str(RPS(player)).replace('RPS.', '') + '.')
+        print("Python chose " + str(RPS(computer)).replace('RPS.', '') + '.')
+        print("")
+
+        def decide_winner(player, computer):
+            nonlocal player_wins
+            nonlocal python_wins
+            if player == 1 and computer == 3:
+                player_wins += 1
+                return "🥳 You win!"
+            elif player == 2 and computer == 1:
+                player_wins += 1
+                return "🥳 You win!"
+            elif player == 3 and computer == 2:
+                player_wins += 1
+                return "🥳 You win!"
+            elif player == computer:
+                return "😲 Tie game!"
+            else:
+                python_wins += 1
+                return "🐍 Python wins!"
+            
+        game_result = decide_winner(player, computer)
+        
+        print(game_result)
+        
+        nonlocal game_count
+        game_count += 1
+        
+        print("\nGame count: " + str(game_count))
+        print("\nPlayer wins: " + str(player_wins))
+        print("\nPython wins: " + str(python_wins))
+        
+        print("\n Play again? ")
+        
+        while True:
+            playagain = input("\n Y for Yes or \n Q for Quit \n")
+            if playagain.lower() not in ["y", "q"]:
+                continue 
+            else:
+                break
+            
+        if playagain.lower() == "y":
+            return play_rps()
+        else:
+            print("\n💕 Thank you for playing!\n")
+            sys.exit("Bye! 👋")
+                
+    return play_rps
+
+play = rps()
+
+play()
